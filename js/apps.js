06c806a63bf2ced142c65e28df7a22a03dab5b85
Jamie Maxwell <jdsmax19@gmail.com>
2020-02-21 16:17:14 +0000

    addedgrid

--- js/apps.js
+++ js/apps.js
@@ -1,13 +1,16 @@
 function hogwartsGame() { 
   
   const width = 20
-  const easyGrid = document.querySelector('.grid')
+  const easyGrid = document.querySelector('.easygrid')
   const gridSize = width ** 2
   const cells = []
-  let Harry
+  const harry = 389
+  // const dementors = [43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59,
+  //   60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78]
 
   function homePage() {
     document.querySelector('.body').style.visibility = 'visible'
+    document.querySelector('.easygrid').style.visibility = 'hidden'
     document.querySelector('.divinstructions').style.display = 'none'
     const bodyImageButtons = document.querySelectorAll('.bodyimagebutton')
     for (const bodyImageButton of bodyImageButtons) {
@@ -31,6 +34,7 @@ function hogwartsGame() {
       document.querySelector('.divlevels').style.visibility = 'hidden'
       document.querySelector('.bodyimages').style.visibility = 'hidden'
       body.classList.add('mainimg')
+      gameStart()
     })
   }
   const instructions = document.querySelector('.instructions')
@@ -40,13 +44,26 @@ function hogwartsGame() {
     document.querySelector('.divlevels').style.visibility = 'hidden'
     document.querySelector('.bodyimages').style.visibility = 'hidden'
   })
-  const exit = document.querySelector('.exit')
-  exit.addEventListener('click', () => {
+  const exitInstructions = document.querySelector('.exit')
+  exitInstructions.addEventListener('click', () => {
     document.querySelector('.divinstructions').style.display = 'none'
     document.querySelector('.title').style.visibility = 'visible'
     document.querySelector('.divlevels').style.visibility = 'visible'
     document.querySelector('.bodyimages').style.visibility = 'visible'
   })
+
+  function gameStart() {
+    document.querySelector('.easygrid').style.visibility = 'visible'
+    for (let i = 0; i < gridSize; i++) {
+      const cell = document.createElement('div')
+      cell.classList.add('cell')
+      if (i === harry) {
+        cell.classList.add('harry')
+      }
+      easyGrid.appendChild(cell)
+      cells.push(cell)
+    }
+  }
 }
 
 window.addEventListener('DOMContentLoaded', hogwartsGame)
